0bf8d270075022d4c54f131b4dc92fdb8ac9e32e
ManOfCheese <36536353+ManOfCheese@users.noreply.github.com>
2018-02-18 23:49:38 +0100

    Cheese Land v.0.2.1
    Made a new terrain that looks like the moon and deleted the old one. Fleshed out the LiftOff script so that it is now more of an enemybehaviour script for the cheese wheel enemy type. Also added two new materials, one cheese material with texture and one without. Deleted the knights because they were hilarious but weren't cheesy enough.

--- Cheese Land Chronicles/Assets/Scripts/LiftOff.cs
+++ Cheese Land Chronicles/Assets/Scripts/LiftOff.cs
@@ -3,32 +3,62 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LiftOff : MonoBehaviour {
+    //Variables for controllin initial lift off.
+    public float rotateSpeed; //Sets the base rotate speed of the object. This value increases over time by the amount specified in the rotateSpeedMultiplier.
+    public float rotateSpeedModifier; //Determines by what factor the rotateSpeed increases per frame.
+    private float liftOffSpeed; //Sets the speed at which the object lifts off from the ground. Is determined by the rotatespeed.
+    public float liftOffSpeedModifier; //Determines what percentage of the rotateSpeed constitutes the liftOffSpeed.
 
-    public float rotateSpeed;
-    private float liftOffSpeed;
-    public float rotateSpeedMultiplier;
-    private int timer;
+    //Variables for controlling the minimum and maximum rotate speeds.
+    public int minRotateSpeed; //Determines the minimum rotateSpeed required for lift off.
+    public int maxRotateSpeed; //Determines the maximum rotateSpeed that can be reached.
+
+    //Variables for controlling the target and speed of the attack.
+    public Transform target; //Deternines the target of the objects horizontal flight.
+    public float speed; //Determines the speed of the objects horizontal flight.
+
+    //Variables for controlling how high the object will fly
+    public float maxHeight; //Sets the maximum height the object will lift off the ground compared to its starting height.
+    private float heightIncrease = 0;
+
+    //Variables for controlling the delay timer.
+    public int timerMin; //Minimum amount for the timer.
+    public int timerMax; //Maximum amount for the timer.
+    private int timer; //A timer to randomize when the object starts lifting off.
 
     private void Start()
     {
-        timer = Random.Range(10, 500);
+        timer = Random.Range(timerMin, timerMax); //Set the timer to a random number between 10 and 500.
     }
 
-    // Update is called once per frame
     void Update ()
     {
-        timer -= 1;
-        if (timer <= 0)
+        //Lifting off
+        timer -= 1; //Subracts 1 from the timer varaible each frame.
+        if (timer <= 0) //If the time raches zero
         {
-            transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y + rotateSpeed, transform.eulerAngles.z);
-            if (rotateSpeed <= 50)
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y + rotateSpeed, transform.eulerAngles.z); //Start rotating in the y direction by an amount of degrees specified by the rotateSpeed variable
+            if (rotateSpeed <= maxRotateSpeed) //If the rotate speed is below 50 
             {
-                liftOffSpeed = rotateSpeed * 0.2f;
-                rotateSpeed *= rotateSpeedMultiplier;
+                liftOffSpeed = rotateSpeed * liftOffSpeedModifier; //set the liftoff speed to the rotateSpeed * the liftOffSpeedModifier.
+                rotateSpeed *= rotateSpeedModifier; //multiply the rotateSpeed by the rotateSpeedModifier.
             }
-            if (rotateSpeed >= 20 && rotateSpeed <= 50)
+            if (rotateSpeed >= minRotateSpeed && heightIncrease <= maxHeight) //If the rotateSpeed is above the minRotateSpeed and the current x position is below the starting x value + the max x value.
             {
-                transform.position += Vector3.up * Time.deltaTime * liftOffSpeed;
+                transform.position += Vector3.up * Time.deltaTime * liftOffSpeed; //Move the object up by the value of liftOffSpeed each frame.
+                heightIncrease += Time.deltaTime * liftOffSpeed; //Update the height increase so we know how far we have moved on the y axis.
+            }
+        }
+
+        //Flying towards a target
+        float step = speed * Time.deltaTime; //determines the steps in which the Vector3.moveTowards moves towards it's target.
+
+        if (heightIncrease >= maxHeight) //if the rotateSpeed is at or above its maximum
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, step); //Move towards the target
+            if (transform.position == target.position) //If the target is reached
+            {
+                Destroy(this.gameObject); //DIE
             }
         }
 	}
